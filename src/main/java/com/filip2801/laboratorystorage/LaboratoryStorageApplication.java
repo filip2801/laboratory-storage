b1b69942efd97d102ff7b7525e1f579769527b14
Filip Karas <filip@netop.cloud>
2023-08-16 01:08:18 +0200

    Configure project

---
+++ src/main/java/com/filip2801/laboratorystorage/LaboratoryStorageApplication.java
@@ -0,0 +1,13 @@
+package com.filip2801.laboratorystorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratoryStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboratoryStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filip2801/laboratorystorage/LaboratoryStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filip2801.laboratorystorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratoryStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

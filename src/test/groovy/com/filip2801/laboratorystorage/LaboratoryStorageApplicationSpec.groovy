8e6d022db5085a622769aac5c6201823cbeff8fc
Filip Karas <filip@netop.cloud>
2023-08-19 02:08:02 +0200

    Fix test on gitlab

--- src/test/groovy/com/filip2801/laboratorystorage/LaboratoryStorageApplicationSpec.groovy
+++ src/test/groovy/com/filip2801/laboratorystorage/LaboratoryStorageApplicationSpec.groovy
@@ -1,14 +1,9 @@
 package com.filip2801.laboratorystorage
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.ApplicationContext
-import org.springframework.test.context.ContextConfiguration
-import spock.lang.Specification
 
-@ContextConfiguration
-@SpringBootTest
-class LaboratoryStorageApplicationSpec extends Specification {
+class LaboratoryStorageApplicationSpec extends IntegrationTestSpecification {
 
     @Autowired
     ApplicationContext webApplicationContext
